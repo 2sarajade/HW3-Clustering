32407789148143ff392af6fefd590cf14fd1c6a8
2sarajade <2sarajade@gmail.com>
2024-01-30 13:12:12 -0800

    silhouette scoring

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -24,3 +24,29 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+        scores = []
+        for i in range(len(y)):
+            a = self._calculate_a(i, X, y)
+            b = self._calculate_b(i, X, y)
+            max_ab = max(a, b)
+            scores.append((b-a)/max_ab)
+        return scores
+
+    def _calculate_a(self, i, X, y):
+        i_cluster = y[i]
+        i_point = X[i,]
+        distances = [np.linalg.norm(i_point-X[point,]) for point in range(len(y)) if point != i and y[point] == i_cluster]
+        return np.mean(distances)
+    
+    def _calculate_b(self, i, X, y):
+        i_cluster = y[i]
+        i_point = X[i,]
+        b_values = []
+        for cluster_label in np.unique(y):
+            if cluster_label != i_cluster:
+                distances = [np.linalg.norm(i_point-X[point]) for point in range(len(y)) if y[point] == cluster_label]
+                b_values.append(np.mean(distances))
+        if b_values:
+            return min(b_values)
+        else:
+            return 0.0
